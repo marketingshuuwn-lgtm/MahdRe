ea934d6d25ee818bcd909c21c2103fcea9a3d303
marketingshuuwn-lgtm <marketingshuuwn@gmail.com>
2026-07-28 00:29:12 +0300

    feat(archive): import archive.css

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { ToastProvider } from './hooks/useToast.jsx';
 import './index.css';
 import './styles/trello.css';
 import './styles/workspaces.css';
+import './styles/archive.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
